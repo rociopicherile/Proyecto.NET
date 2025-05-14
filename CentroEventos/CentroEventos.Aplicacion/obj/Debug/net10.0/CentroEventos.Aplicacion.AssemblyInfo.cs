8095fb10d6a3ed9e01e89f3dbc7c8c8202f548bc
Matias <caubetmatias@gmail.com>
2025-05-14 16:59:02 -0300

    Agregue el useCase de listar eventos deportivos solo con cupo disponible - FALTA HACER EL CUERPO

--- CentroEventos/CentroEventos.Aplicacion/obj/Debug/net10.0/CentroEventos.Aplicacion.AssemblyInfo.cs
+++ CentroEventos/CentroEventos.Aplicacion/obj/Debug/net10.0/CentroEventos.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4da1f8e092d5ba93b62bfdf808a5c7f6dd3fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394588be83a16bbbf0c554d40ac14b7e97c89765")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
